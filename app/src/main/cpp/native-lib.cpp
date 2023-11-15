37a7d4412fc1c90c51c4e5c344fc2f81c2fc09c9
sawpla10 <dnjswo0606@gmail.com>
2023-11-15 13:13:47 +0900

    ss

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -62,3 +62,61 @@ Java_com_example_visionproject_SafetyModeActivity_ConvertSafe(JNIEnv *env, jobje
     return mat_addr_input;
     // TODO: implement ConvertSafe()
 }
+
+
+
+/*#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+int main()
+{
+    VideoCapture cap(0); // 웹캠에서 입력을 받습니다.
+    if (!cap.isOpened())
+    {
+        return -1;
+    }
+
+    Mat frame, gray, frameDelta, thresh, firstFrame;
+    vector<vector<Point> > cnts;
+    cap >> firstFrame;
+    cvtColor(firstFrame, firstFrame, COLOR_BGR2GRAY);
+    GaussianBlur(firstFrame, firstFrame, Size(21, 21), 0);
+
+    while (true)
+    {
+        cap >> frame;
+        if (frame.empty())
+            break;
+
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        GaussianBlur(gray, gray, Size(21, 21), 0);
+
+        absdiff(firstFrame, gray, frameDelta);
+        threshold(frameDelta, thresh, 25, 255, THRESH_BINARY);
+
+        dilate(thresh, thresh, Mat(), Point(-1,-1), 2);
+        findContours(thresh.clone(), cnts, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+        for (int i = 0; i < cnts.size(); i++)
+        {
+            if (contourArea(cnts[i]) < 500)
+            {
+                continue;
+            }
+
+            putText(frame, "Motion Detected", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+        }
+
+        imshow("Camera", frame);
+
+        if (waitKey(1) == 27)
+        {
+            // ESC 키를 누르면 루프에서 빠져나옵니다.
+            break;
+        }
+    }
+
+    return 0;
+}*/
